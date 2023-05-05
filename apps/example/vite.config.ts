8a1fb477c42f6546c07e7c6d70079399beed268d
Kasey Culmback <kasey.culmback@gmail.com>
2023-05-05 14:34:03 -0700

    fix: update base url and favicon

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/react-chrome-dino-ts/',
-})
+  base: command === 'build' ? '/react-chrome-dino-ts/' : '/',
+}))
